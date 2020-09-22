0557538e7086427b8cfa7daadf5f399ecab8a55b
Jeffery Huang <jeffery.huang@itnsl.net>
2020-09-22 14:21:49 +0800

    lp295A_v2_init

---
+++ prdTesting/public/js/ws-index-url.js
@@ -0,0 +1,3 @@
+const WS_BASE_URL = "wss://api.mircoinfolab.com/websocket";
+const HTTP_BASE_URL = "https://api.mircoinfolab.com";
+const TECH_ANA_URL = "https://api.bingxuegirl.com/inv/currencies";
