e00214ba8f1592ce33658f7eb7b3595c671993df
Jeffery Huang <jeffery.huang@itnsl.net>
2020-10-30 16:27:51 +0800

    30day

--- 917_需求227(lp297G)/public/js/ws-index-url.js
+++
@@ -1,3 +0,0 @@
-const WS_BASE_URL = "wss://api.mircoinfolab.com/websocket";
-const HTTP_BASE_URL = "https://api.mircoinfolab.com";
-const TECH_ANA_URL = "https://api.bingxuegirl.com/inv/currencies";
