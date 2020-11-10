4f92402af93c3007ddca80abc6e4d04fe9906f15
Jeffery Huang <jeffery.huang@itnsl.net>
2020-11-10 18:16:16 +0800

    gs20

---
+++ prdTesting/promote/S02/js/s02.js
@@ -0,0 +1,22 @@
+wow = new WOW({
+    animateClass: 'animated',
+    offset: 100,
+});
+wow.init();
+
+
+$(window).scroll(function() {
+
+});
+
+$(window).resize(function() {
+
+});
+
+
+
+$(document).ready(function() {
+    remCompute(750, true);
+
+
+});
