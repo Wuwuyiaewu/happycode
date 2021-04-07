a8d72ebee907105c4885e1f41b46ee518c9915d6
Wuwuyiaewu <okman9527@gmail.com>
2021-04-07 18:17:28 +0800

    66

--- js/js_beinput.js
+++ js/js_beinput.js
@@ -1,26 +1,3 @@
-function main(require) {
-    var obj = require('./utils')
-    console.log(obj.cal(30))
-    console.log(obj.name)
-}
-
-function utils(module) {
-    function calculate(n) {
-        return ((n * 100 + 20 - 4)) % 10 + 3
-    }
-
-    module.exports = {
-        cal: calculate,
-        name: 'hello'
-    }
-}
-
-var m = {}
-utils(m)
-
-// 加入底下這幾行
-function r() {
-    // 回傳我們所需要的 m.exports
-    return m.exports
-}
-main(r)
+import obj from './js_input.js'
+console.log(obj.cal(30))
+console.log(obj.name)
--- js/js_input.js
+++ js/js_input.js
@@ -2,7 +2,7 @@ function calculate(n) {
     return ((n * 100 + 20 - 4)) % 10 + 3 // 計算價格公式
 }
 
-out = {
+export default {
     cal: calculate,
     name: 'hello'
 }
