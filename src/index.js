750f0d3b7afdb2336dd69e3f8ece2a6b3433a3cb
Wuwuyiaewu <okman9527@gmail.com>
2021-04-08 18:26:23 +0800

    jsonp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-const getText = () => 'JavaScript ES6';
-
-document.getElementById('text').innerHTML = getText();
+fetch('https://randomuser.me/api/', {})
+    .then((response) => {
+        // 這裡會得到一個 ReadableStream 的物件
+        console.log(response);
+        // 可以透過 blob(), json(), text() 轉成可用的資訊
+        return response.json();
+    }).then((jsonData) => {
+        console.log(jsonData);
+    }).catch((err) => {
+        console.log('錯誤:', err);
+    });
