cc7e3c007e1ba0b383f64913314d7304f558178c
Wuwuyiaewu <okman9527@gmail.com>
2021-05-14 18:23:31 +0800

    router_revue

--- unify_web_new-master_0428/src/pages/mobile/views/home.vue
+++ unify_web_new-master_0428/src/pages/mobile/views/home.vue
@@ -1,10 +1,13 @@
 <template>
-    <div>
-    </div>
+    <div></div>
 </template>
 
 <script>
-
+import geoPermission from '@/pages/mobile/mixins/geoPermission.js'
+export default {
+    name: 'Home',
+    mixins: [geoPermission]
+}
 </script>
 
 <style lang="scss" scoped>
